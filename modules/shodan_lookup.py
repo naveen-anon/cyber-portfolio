02ac6d137750ca7a3b38cebcca9088ff957fd660
Naveen khatri <naveen1khatri@gmail.com>
2026-03-14 09:08:56 +0530

    Update shodan_lookup.py

--- modules/shodan_lookup.py
+++ modules/shodan_lookup.py
@@ -1,1 +1,17 @@
 
+import shodan
+from config import SHODAN_API
+
+def shodan_host(ip):
+
+    api = shodan.Shodan(LSkyFO1W4JBevcT5yZoOJXMiCgOZIjqx)
+
+    try:
+
+        host = api.host(ip)
+
+        return host
+
+    except:
+
+        return {"error":"host not found"}
