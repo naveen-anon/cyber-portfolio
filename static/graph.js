39b1e39d3adcf65a5a8ff921c9028a516a8e0cf3
Naveen khatri <naveen1khatri@gmail.com>
2026-03-14 09:13:41 +0530

    Update graph.js

--- static/graph.js
+++ static/graph.js
@@ -1,1 +1,19 @@
 
+var nodes = new vis.DataSet([
+{ id:1,label:"Target"},
+{ id:2,label:"Email"},
+{ id:3,label:"IP"},
+{ id:4,label:"Domain"}
+])
+
+var edges = new vis.DataSet([
+{from:1,to:2},
+{from:1,to:3},
+{from:1,to:4}
+])
+
+var container = document.getElementById("network")
+
+var data = {nodes:nodes,edges:edges}
+
+new vis.Network(container,data,{})
