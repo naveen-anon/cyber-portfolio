6428795a72ed24b0e228bdcc2fdecff4c1b5cdb8
Naveen khatri <naveen1khatri@gmail.com>
2026-03-14 09:52:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
 
+# Shodan API key
+SHODAN_API = "LSkyFO1W4JBevcT5yZoOJXMiCgOZIjqx"
+
+# OpenAI API key
+OPENAI_API = "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"
+
+# Flask secret key (login/session के लिए)
+SECRET_KEY = "cyber_osint_secret"
