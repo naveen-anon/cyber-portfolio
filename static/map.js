355dce18db9ba6a6c473e21a2b4fe79f5c9eb972
Naveen khatri <naveen1khatri@gmail.com>
2026-03-14 09:13:25 +0530

    Update map.js

--- static/map.js
+++ static/map.js
@@ -1,1 +1,16 @@
 
+const world = Globe()
+
+(document.getElementById("globe"))
+
+.globeImageUrl("//unpkg.com/three-globe/example/img/earth-dark.jpg")
+
+.pointsData([
+{lat:28.6,lng:77.2},
+{lat:37.7,lng:-122.4},
+{lat:35.6,lng:139.6}
+])
+
+.pointAltitude(0.1)
+
+.pointColor(()=> "red")
